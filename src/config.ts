f68bfa68a90f7a5a3d4a7b863c6b5710f7c4e75b
Martin Kalabek <martin.kalabek@deckers.com>
2019-10-01 15:22:05 +0100

    Convert config to TypeScript too

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+// Generic
+export const DEBUG: number = 1
+
+// Page
+export const RELOAD_TIMER: number = 0
+
+// Browser
+export const BROWSER_USERAGENT: string = 'Mozilla/5.0 (Windows NT 6.2 WOW64) AppleWebKit/538.1 (KHTML, like Gecko) Safari/538.1'
+export const BROWSER_VIEW: number[] = [1920, 1080]
+
+// Project
+export const PAGE_HOME: string = ''
